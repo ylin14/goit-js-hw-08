43242ed18ca3e6446817b8bdc9842a077119850d
ylin14 <y.linyvyi@gmail.com>
2023-04-29 15:40:33 +0300

    gallery via lightbox is done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const ref = {
+    gallery: document.querySelector('.gallery'),
+  };
+
+
+  function createGalleryMarkup(arr) {
+    return arr
+      .map(
+        ({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`,
+      )
+      .join('');
+  };
+  
+  ref.gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+  const lightbox = new SimpleLightbox('.gallery a', { 
+    captionDelay: 250,
+    captionsData: "alt",
+   });
