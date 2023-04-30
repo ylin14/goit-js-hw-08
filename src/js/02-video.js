45542040a101df35d77d19c02a87f610f3425351
ylin14 <y.linyvyi@gmail.com>
2023-05-01 00:16:11 +0300

    done hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,44 @@
 import Player from '@vimeo/player';
-import {save, load} from './storage' 
+import throttle from 'lodash.throttle';
 // 1. Ініціалізувати плєр.
 // 2. Методом on() відстежити подію "timeupdate".
 // 3. Написати функцію для збереження часу відтворення у локальне сховище. (час брати із aria-value-now?)
 // 4. При перезавантаженні сторінки через метод setCurrentTime() відновлювати відео на моменті зупинки.
-// 5. Додати lodash.throttle.
+// 5. Додати npm i lodash.throttle.
 // 6. Оновлювати час відтворення у сховищі не частіше, ніж раз на секунду.
 // aria-valuenow атрибут, який показує час відео.
 
 const STORAGE_KEY = "videoplayer-current-time";
 
-const player = new Player('vimeo-player');
+const iframe = document.getElementById("vimeo-player");
+const player = new Player(iframe);
 
 
-player.on('timeupdate', function() {
-    console.log('played the video!');
-});
+function onTimeUpd (dataTime) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataTime));
+}
+
+player.on('timeupdate', throttle(onTimeUpd, 1000));
+
+function getSavedTime () {
+    const savedTime = localStorage.getItem(STORAGE_KEY);
+    
+    if(!savedTime) {
+        return
+    }
+
+    const parsedSavedTime = JSON.parse(savedTime);
+
+    return parsedSavedTime.seconds
+}
+
+
+const currentTime = getSavedTime() ? getSavedTime() : 0;
+
+player.setCurrentTime(currentTime);
+
+
+
 
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,65 @@
+import throttle from "lodash.throttle";
+
+const formRef = document.querySelector(".feedback-form");
+const emailRef = document.querySelector("label>input");
+const messageRef = document.querySelector("label>textarea");
+const STORAGE_KEY = "feedback-form-state";
+
+
+
+function onFormInput(e) {
+    const dataObj = createObj(emailRef.value, messageRef.value);
+
+    addObjToLocalStorage(dataObj)
+}
+
+function createObj (email, message) {
+    return {
+        email,
+        message
+    }
+}
+
+
+function addObjToLocalStorage (obj) {
+    const strigifiedObj = JSON.stringify(obj);
+    localStorage.setItem(STORAGE_KEY, strigifiedObj);
+}
+
+
+function getSavedData () {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    
+    if(!savedData) {
+        return {
+            email: "",
+            message: "",
+        }
+    }
+
+    return JSON.parse(savedData);
+    
+}
+
+function setDataToForm () {
+    const {email, message} = getSavedData ();
+    emailRef.value = email;
+    messageRef.value = message;
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    console.log(getSavedData ());
+
+    emailRef.value = "";
+    messageRef.value = "";
+    localStorage.clear();
+}
+
+
+setDataToForm ();
+
+formRef.addEventListener("input", throttle(onFormInput, 500));
+formRef.addEventListener("submit", onFormSubmit);
+
 
